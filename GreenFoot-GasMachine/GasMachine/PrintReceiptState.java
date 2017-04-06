98f320b57e20a06d48cc1b0e789391970bd7d192
Laura Zhou <laurajoe310@yahoo.com>
2017-04-06 11:26:57 -0700

    added Message object in PrintReceiptState.java.

--- GreenFoot-GasMachine/GasMachine/PrintReceiptState.java
+++ GreenFoot-GasMachine/GasMachine/PrintReceiptState.java
@@ -9,31 +9,51 @@ import greenfoot.*;
 
 public class PrintReceiptState extends State 
 {
-	final private String costMsg = "Your total cost is: ";
-	final private String thankyouMsg = "Thank you!";
+    
+    final private String thankyouMsg = "Thank you!";
+    private String costMsg = "Your total cost is: ";  //needs to append the cost in the end
+    Message message = new Message();
 
-	int cost = 0;
+    int cost = 0;
 
-	/**
+    /**
      * Constructor 
      * 
      * @param cost   total cost on the receipt
      */
-	public PrintReceiptState(int cost) 
-	{
-		this.cost = cost;
-	}
+    public PrintReceiptState(int cost) 
+    {
+        this.cost = cost;
+        costMsg += cost;
+    }
 
-	/**
+    /**
      * Print out the receipt. 
      * 
      */
-	public void print() 
-	{
-		System.out.println(costMsg + cost);
-		
-		//The machine prints out a message “Thank You” after printing the receipt or after a timeout of 30 seconds.
-		Greenfoot.delay(30);
-		System.out.println(thankyouMsg);
-	}
+    public void print() 
+    {
+        setMessage(costMsg);                // on canvas
+        System.out.println(costMsg);        // in cmd
+        
+        //The machine prints out a message “Thank You” after printing the receipt or after a timeout of 30 seconds.
+        Greenfoot.delay(30);
+        
+        setMessage(thankyouMsg);            // on canvas
+        System.out.println(thankyouMsg);    // in cmd
+    }
+    
+    public void setMessage(String msg) {
+        MouseInfo mouseinfo = Greenfoot.getMouseInfo();
+        int mouseX = mouseinfo.getX();
+        int mouseY = mouseinfo.getY();
+        
+        World world = getWorld();
+        if(message.getWorld() != null) {
+            world.removeObject(message);
+        }
+        
+        world.addObject(message, mouseX, mouseY);
+        message.setText(msg);
+    }
 }
