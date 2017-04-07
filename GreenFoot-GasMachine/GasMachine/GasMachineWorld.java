f674ae722de3899f753f94dd7ac0c81d4e40162e
rashmishrm <rashmishrm74@gmail.com>
2017-04-06 18:01:21 -0700

    Design ui for KeyPad

--- GreenFoot-GasMachine/GasMachine/GasMachineWorld.java
+++ GreenFoot-GasMachine/GasMachine/GasMachineWorld.java
@@ -39,5 +39,41 @@ public class GasMachineWorld extends World
         
         addObject(new Nozzle(), 500, y);
         
+       
+        KeyPadButton[][] keyPadMatrix = new KeyPadButton[4][4];
+        
+        keyPadMatrix[0][0]=new KeyPadButton1();
+        keyPadMatrix[0][1]=new KeyPadButton2();
+        keyPadMatrix[0][2]=new KeyPadButton3();
+        keyPadMatrix[0][3]=new KeyPadButtonClear();
+        
+        
+        keyPadMatrix[1][0]=new KeyPadButton4();
+        keyPadMatrix[1][1]=new KeyPadButton5();
+        keyPadMatrix[1][2]=new KeyPadButton6();
+        keyPadMatrix[1][3]=new KeyPadButtonEnter();
+        
+        keyPadMatrix[2][0]=new KeyPadButton7();
+        keyPadMatrix[2][1]=new KeyPadButton8();
+        keyPadMatrix[2][2]=new KeyPadButton9();
+        keyPadMatrix[2][3]=new KeyPadButton();
+        
+        keyPadMatrix[3][0]=new KeyPadButton();
+        keyPadMatrix[3][1]=new KeyPadButton0();
+        keyPadMatrix[3][2]=new KeyPadButton();
+        keyPadMatrix[3][3]=new KeyPadButton();
+        
+          x = 300;
+         y = 200;
+         for (int r = 0; r < 4; r++) {
+             for (int c = 0; c < 4; c++) { 
+            addObject(keyPadMatrix[r][c], x, y);
+            x+= 50;
+        }
+        y+=50;
+        x=300;
+        }
+        
+        
     }
 }
